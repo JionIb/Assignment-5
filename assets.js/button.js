c3e7133256a29d22d5b8f22f329853fe1af5759a
my-first-github-ripo <jionmolla261@gmail.com>
2025-03-28 03:06:59 +0600

    fifth assi

---
+++ assets.js/button.js
@@ -0,0 +1,71 @@
+document.getElementById('btnA-1').addEventListener('click',function
+    (event){
+        event.preventDefault()
+
+        alert("Board updated Successfully.");
+
+        this.disabled = true;
+        
+    }
+
+)
+
+document.getElementById('btnA-2').addEventListener('click',function
+    (event){
+        event.preventDefault()
+
+        alert("Board updated Successfully.");
+
+        this.disabled = true;
+        
+    }
+
+)
+
+document.getElementById('btnA-3').addEventListener('click',function
+    (event){
+        event.preventDefault()
+
+        alert("Board updated Successfully.");
+
+        this.disabled = true;
+        
+    }
+
+)
+
+document.getElementById('btnA-4').addEventListener('click',function
+    (event){
+        event.preventDefault()
+
+        alert("Board updated Successfully.");
+
+        this.disabled = true;
+        
+    }
+
+)
+
+document.getElementById('btnA-5').addEventListener('click',function
+    (event){
+        event.preventDefault()
+
+        alert("Board updated Successfully.");
+
+        this.disabled = true;
+        
+    }
+
+)
+
+document.getElementById('btnA-6').addEventListener('click',function
+    (event){
+        event.preventDefault()
+
+        alert("Board updated Successfully.");
+
+        this.disabled = true;
+        
+    }
+
+)
--- assets.js/them.js
+++ assets.js/them.js
@@ -1,9 +1,14 @@
-document.getElementById("them").addEventListener('click',function(event){
+document.getElementById("them").addEventListener('click', function(event) {
     event.preventDefault();
-    document.body.style.backgroundColor = 'lightblue' ;
-   
-}
-)
-
-
+    
+    const colors = ['lightblue', 'lightgreen', 'lightpink', 'lavender', 'lightyellow', 'lightcyan','red'];
+    
+    const currentColor = document.body.style.backgroundColor;
+    
+    let currentIndex = colors.indexOf(currentColor);
+    if (currentIndex === -1) currentIndex = 0;
+    
+    const bgColour = (currentIndex + 1) % colors.length;
+    document.body.style.backgroundColor = colors[bgColour];
+});
 
